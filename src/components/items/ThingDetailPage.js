cc7d7460526996f84903f32a5c5053b9a18a758c
magmadude <8989953+magmadude20@users.noreply.github.com>
2026-03-12 22:32:05 -0500

    Fix compile error. hopefully.

--- src/components/items/ThingDetailPage.js
+++ src/components/items/ThingDetailPage.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { supabase } from '../../lib/supabaseClient';
 import { deleteItem, updateItem } from '../../services/itemsService';
 import { addShare, removeShare, setThingGroups } from '../../services/thingsToGroupsService';
 import Owner from '../user/Owner';
